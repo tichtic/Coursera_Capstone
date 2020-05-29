a9d7a6848ffb1a38b12c900610a1da879f6d762d
tichtic <38280385+tichtic@users.noreply.github.com>
2020-05-29 15:31:36 +0700

    Add files via upload

---
+++ Cong.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In this assignment, you will be asked to create a new repository on your Github account, and to create a Jupyter Notebook and submit a shareable link to it for peer evaluation.
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# <h1> Hello Capstone Project Course! <h1>
+
+# In[ ]:
+
+
+
+
